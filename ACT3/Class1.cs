36e883061bfcbf16b64eff2341bf074af2be2d25
LOL.2 <180150@site.asty-moulin.be>
2022-12-02 11:22:13 +0100

    Ajoutez des fichiers projet.

---
+++ ACT3/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACT3
+{
+    internal class Class1
+    {
+        private string _nom;
+        private uint _Oreilles;
+
+        public Class1(string nom, uint Oreilles)
+        {
+            _nom = nom;
+            _Oreilles = Oreilles;
+        }
+
+       
+
+       
+
+        public void LectureMessage(string message, Class1 quiDit)
+        {
+            Console.WriteLine(_nom + " a entendu un message \n" + quiDit._nom + " a dit : " + message);
+        }
+
+        public string AfficherElephant()
+        {
+            return _nom + " mes oreilles  font " + _Oreilles.ToString() + " cm";
+        }
+        public void Message(string message, Class1 quiRecoit)
+        {
+            quiRecoit.LectureMessage(message, this);
+        }
+        public uint AvoirOreilles()
+        {
+            return _Oreilles;
+        }
+    }
+}
---
+++ ACT3/Program.cs
@@ -0,0 +1,86 @@
+﻿using ACT3;
+using System;
+
+namespace ACT3_Elephants
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string choice = "";
+
+           
+            Class1[] Elephants = new  Class1[2];
+            Elephants[0] = new  Class1("Zazou", 100);
+            Elephants[1] = new  Class1("Titi", 120);
+            Class1[] AutreElephants = new  Class1[7];
+            AutreElephants[0] = new  Class1("1", 134);
+            AutreElephants[1] = new  Class1("2", 96);
+            AutreElephants[2] = new  Class1("3", 50);
+            AutreElephants[3] = new  Class1("4", 78);
+            AutreElephants[4] = new  Class1("5", 26);
+            AutreElephants[5] = new  Class1("6", 10);
+            AutreElephants[6] = new  Class1("7", 5);
+
+            Console.WriteLine("Choisissez \n 1 pour afficher les info de zazou \n 2 pour afficher les info de titi \n 3 pour les échanger \n 4 pour vois le transfer d'un message de zazou à titi \n 5 pour reviser la notion de tableau et l'appliquer aux objets");
+            choice = Console.ReadLine();
+
+            Choix(choice, Elephants, AutreElephants);
+            Console.ReadLine();
+        }
+        static void Choix(string choix, Class1[] Elephants, Class1[] AutreElephants)
+        {
+            string exist = "je suis ";
+            switch (choix)
+            {
+                case "1":
+                    exist += Elephants[0].AfficherElephant();
+                    break;
+
+                case "2":
+                    exist += Elephants[1].AfficherElephant();
+                    break;
+
+                case "3":
+                    Class1 cacheElephant = Elephants[0];
+                    Elephants[0] = Elephants[1];
+                    Elephants[1] = cacheElephant;
+                    exist += Elephants[0].AfficherElephant();
+                    exist += "\n";
+                    exist += Elephants[1].AfficherElephant();
+                    break;
+
+                case "4":
+                    Elephants[1].Message("Bonjour", Elephants[0]);
+                    Elephants[0].LectureMessage("Bonjour", Elephants[1]);
+                    exist = null;
+                    break;
+
+                case "5":
+                    exist += VerifOreilles(AutreElephants).AfficherElephant();
+                    break;
+            }
+            static Class1 VerifOreilles(Class1[] AutreElephants)
+        {
+            
+            int IE = 0;
+            uint LaPlusGrandOreilles = 0;
+            for (int i = 0; i < AutreElephants.Length; i++)
+            {
+                if (LaPlusGrandOreilles < AutreElephants[i].AvoirOreilles())
+                {
+                    LaPlusGrandOreilles = AutreElephants[i].AvoirOreilles();
+                    IE = i;
+                }
+            }
+
+            return AutreElephants[IE];
+        }
+            Console.WriteLine(exist);
+
+
+
+        }
+    }
+}
